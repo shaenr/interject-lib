9b1e62adf22f1adcb1d9bf832668873823d19620
viruFac <virufac@protonmail.com>
2019-09-20 18:51:19 -0500

    initialize

---
+++ interject.py
@@ -0,0 +1,19 @@
+from string import Template
+from pathlib import Path
+
+p = Path("interject.txt").absolute()
+
+interject = Template(p.read_text())
+
+linux = input("Linux \n> ").capitalize()
+gnu = input("GNU \n> ").capitalize()
+corelibs = input("made useful by the... 1 \n> ")
+shell_utilities = input("made useful by the... 2 \n> ")
+system_components = input("made useful by the... 3 \n> ")
+os = input("...comprising a... \n> ")
+posix = input("as defined by... \n> ")
+kernel = input("kernel \n> ")
+
+adlib = interject.substitute(linux=linux, gnu=gnu, corelibs=corelibs, shell_utilities=shell_utilities, system_components=system_components, os=os, posix=posix, kernel=kernel)
+
+print(adlib)
